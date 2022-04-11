7cd0983a895b66663af1c1cd3c7d8a656751fb24
Gabot3ck <jesus.castilloc@outlook.com>
2022-04-10 21:53:06 -0300

    Se crea el desafio DOM

--- js/main.js
+++ js/main.js
@@ -13,61 +13,28 @@
 // - El algoritmo evaluará si el nickname ingresado ya existe, de ser así el usuario está obligado a ingresar otro.
 // - El array totalEquipos almacenará todos los arrays de los diferentes equipos.
 
+import {equipo, jurado, totalEquipos} from "./dbUsuarios.js";
 
 
 // Declaración de Variables
 let tipo = 0;
-let nickname = "";
+let username = "";
 let nombre = "";
 let apellido = "";
-let edad = "";
-let registro = "";
-
-
-
-//Declaración de Arrays
-
-const jurado = [
-    {tipo: "juez",
-    nickname: "ben",
-    nombre: "Benjamin",
-    apellido: "Torres",
-    edad: 35}
-];
-
-const equipo = [
-    {tipo: "participante",
-    nickname: "warrior.net",
-    nombre: "Julio",
-    apellido: "Castro",
-    edad: 28},
-
-    {tipo: "participante",
-    nickname: "furier",
-    nombre: "Carlos",
-    apellido: "Baeza",
-    edad: 30},
-
-    {tipo: "participante",
-    nickname: "hugox",
-    nombre: "Hugo",
-    apellido: "Paredes",
-    edad: 23}
-];
-
-const totalEquipos = [];
+let email = "";
+const d = document;
 
 
 
 
 //Creación de Clases
 class Usuario{
-    constructor(tipo,nickname,nombre,apellido,edad){
+    constructor(tipo,username,nombre,apellido,email){
         this.tipo = tipo;
-        this.nickname = nickname;
+        this.username = username;
         this.nombre = nombre;
         this.apellido = apellido;
-        this.edad = edad;
+        this.email = email;
     }
 }
 
@@ -87,47 +54,60 @@ function elegirTipo(){
         switch(tipo){
             case 1:
                 tipo = "juez";
-                nickname = buscarNickname().toUpperCase();
+                username = buscarNickname().toUpperCase();
                 nombre += prompt("Ingrese Nombre:").toUpperCase();
                 apellido += prompt("Ingrese Apellido:").toUpperCase();
-                edad += prompt("Ingrese su edad:").toUpperCase();
-                alert(`Bienvenid@! ${nickname}, usted será parte del jurado en el Hackaton CoderHouse 2022`);
+                email += prompt("Ingrese su email:").toUpperCase();
+                // alert(`Bienvenid@! ${username}, usted será parte del jurado en el Hackaton CoderHouse 2022`);
+
+                let containerCardJurado = d.getElementById("containerJurado");
+                let nuevaCard = d.createElement("div");
+
+                nuevaCard.innerHTML = `
+                    <div class="card h-100">
+                        <img src="images/usuario.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${username}</h5>
+                            <p>Nombre:</p><span>${nombre}</span><br>
+                            <p>Apellido:</p><span>${apellido}</span><br>
+                            <p>Email:</p><span>${email}</span>
+                        </div>
+                    </div>`;
+                
+                nuevaCard.classList.add("col");
+                containerCardJurado.appendChild(nuevaCard);
+
                 break;
     
             case 2:
                 tipo = "participante";
-                nickname = buscarNickname().toUpperCase();
+                username = buscarNickname().toUpperCase();
                 nombre += prompt("Ingrese Nombre:").toUpperCase();
                 apellido += prompt("Ingrese Apellido:").toUpperCase();
-                edad += prompt("Ingrese su edad:").toUpperCase();
-                alert(`Bienvenid@s! \n${nicknameParticipantes.concat(nickname).join("\n")}\nustedes serán equipo del Hackaton Coderhouse 2022`);
+                email += prompt("Ingrese su email:").toUpperCase();
+                alert(`Bienvenid@s! \n${nicknameParticipantes.concat(username).join("\n")}\nustedes serán equipo del Hackaton Coderhouse 2022`);
                 break;
         }
 
-    nuevoUser = new Usuario(tipo,nickname,nombre,apellido,edad);
+    nuevoUser = new Usuario(tipo,username,nombre,apellido,email);
 }
 
 
 
-const nicknameParticipantes = equipo.map(el => el.nickname);
-const nicknameJurado = jurado.map(el => el.nickname);
+const nicknameParticipantes = equipo.map(el => el.username);
+const nicknameJurado = jurado.map(el => el.username);
 const almacenarNickname = nicknameParticipantes.concat(nicknameJurado);
-
-//Se muestra por consola el array de todos los nicknames
-console.log("Se muestra el array de los nicknames existentes:\n");
-console.log(almacenarNickname);
-
 
 
 //Se va a evaluar si existe el nickname de un participante al inscribirse
 function buscarNickname(){
 
-    let nick = prompt("Ingresar nickname:");
+    let nick = prompt("Ingresar nombre de usuario:");
 
     while (almacenarNickname.includes(nick)){
 
         alert("Error, el nickname ya existe");
-        nick = prompt("Ingresar nickname:");
+        nick = prompt("Ingresar nombre de usuario:");
     };
 
     almacenarNickname.push(nick);
@@ -135,30 +115,28 @@ function buscarNickname(){
 }
 
 
-//Se crea un temporizador para correr el simulador
-// setTimeout(() => {
-//     elegirTipo();
+// Se crea un temporizador para correr el simulador
+setTimeout(() => {
+    elegirTipo();
 
 
-//     //Se evalua de acuerdo al tipo de usuario si es juez o participante se le agrega a su array correspondiente
-//     if(nuevoUser.tipo === "juez"){
-//         jurado.push(nuevoUser);
-//     } else {
-//         participante = nuevoUser;
-//         equipo.push(participante);
-//     }
+    //Se evalua de acuerdo al tipo de usuario si es juez o participante se le agrega a su array correspondiente
+    if(nuevoUser.tipo === "juez"){
+        jurado.push(nuevoUser);
+    } else {
+        participante = nuevoUser;
+        equipo.push(participante);
+    }
+    totalEquipos.push(equipo)
+    console.log(totalEquipos);
+}, 1200);
 
 
-//     //Se vuelve a mostrar por consola el array de los nicknames con el nuevo nickname ingresado
-//     console.log("Se muestra el array de los nicknames incluyendo el del nuevo usuario:\n");
-//     console.log(almacenarNickname);
-
-//     totalEquipos.push(equipo)
-//     console.log(totalEquipos);
-// }, 1200);
 
 
 /*Reducción de velocidad de video de fondo*/
 let video = document.querySelector('video');
-video.defaultPlaybackRate = 0.5;
+video.defaultPlaybackRate = .3;
 video.load();
+
+

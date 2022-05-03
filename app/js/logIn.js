bd34dff14b2dcc52204acd159125c9498ee0c8e8
Gabot3ck <jesus.castilloc@outlook.com>
2022-05-02 22:21:30 -0300

    Se crean los archivos json

--- app/js/logIn.js
+++ app/js/logIn.js
@@ -64,3 +64,8 @@ btnPass.addEventListener("click",()=>{
 
 
 
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then(response => response.json())
+//     .then(data => console.log(data));
+
